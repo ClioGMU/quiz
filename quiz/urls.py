9add00f3f7c9d663a9ba05f60bf67f153f47d422
lwalterscooper <lwalterscooper@gmail.com>
2019-11-19 15:15:16 -0500

    created quizreview view

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import QuizListView 
+from .views import QuizListView, QuizReviewView
 
 urlpatterns = [
     path('dashboard/', QuizListView.as_view(), name='dashboard'),
+    path('quizreview/<uuid:pk>/', QuizReviewView.as_view(), name='quizreview'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -6,3 +6,8 @@ class QuizListView(LoginRequiredMixin, ListView):
     model = Quiz
     template_name = 'dashboard.html'
     ordering = ['-date_created']
+
+class QuizReviewView(LoginRequiredMixin, DetailView):
+    model = Quiz
+    template_name = 'quizreview.html'
+    ordering = ['-date_created']
