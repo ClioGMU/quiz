7c795e6ef1965ead38702f47428c3695fa16350c
lcrossley <lcrossle@gmu.edu>
2019-11-19 18:29:20 -0500

    added home page

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from .views import QuizListView, QuizReviewView
 
 urlpatterns = [
     path('dashboard/', QuizListView.as_view(), name='dashboard'),
     path('quizreview/<uuid:pk>/', QuizReviewView.as_view(), name='quizreview'),
+    path('', TemplateView.as_view(template_name="home.html"), name='home')
 ]
