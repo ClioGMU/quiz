b2cfc325763914d52b33e9a4923cc8d9342d4b91
lcrossley <lcrossle@gmu.edu>
2019-12-17 15:40:18 -0500

    changed admin url

--- quiz_project/urls.py
+++ quiz_project/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('controls/', admin.site.urls),
     path('', include('quiz.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
